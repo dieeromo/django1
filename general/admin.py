758a7f8b901e4948fc4d5da81e2045ce819d77d2
Diego <diegoromo_408@hotmail.com>
2023-06-08 11:34:51 -0500

    añade base de datos

--- general/admin.py
+++ general/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from . models import empresa, proveedoresProd
 
-# Register your models here.
+
+admin.site.register(empresa)
+admin.site.register(proveedoresProd)
--- general/models.py
+++ general/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class empresa(models.Model):
+    nombreEmpresa = models.CharField(max_length=100)
+    fechaCreacion = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.nombreEmpresa
+
+class proveedoresProd(models.Model):
+    nombreProveedor = models.CharField(max_length=100)
+    fechaCreacion = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.nombreProveedor
